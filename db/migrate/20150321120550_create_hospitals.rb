33c7e2aacfa8fa2c5386e6d57d5302a4469a65d9
aakashkhatter1296 <khatteraakash1996@gmail.com>
2015-03-21 17:39:52 +0530

    initial

---
+++ db/migrate/20150321120550_create_hospitals.rb
@@ -0,0 +1,12 @@
+class CreateHospitals < ActiveRecord::Migration
+  def change
+    create_table :hospitals do |t|
+      t.string :name
+      t.float :latitude
+      t.float :longitude
+      t.int :amb_count
+
+      t.timestamps null: false
+    end
+  end
+end
